e400858108769afd62326fba543d126a63cf0225
Enzo Dayrit <enzodayrit11@gmail.com>
2013-05-28 01:10:24 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NapiWeb::Application.config.secret_token = '4bf72da09434f4781e3d0157f6f673daf33b391805b2feda08110e6eaa045770f0f6e824330cab05f0e87d8b6ec1eb6b44bf35e6f1ccc3f847488e2399ef7d97'
