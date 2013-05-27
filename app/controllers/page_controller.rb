a98ee1eba2f21f532b745a07385fdb3835a319d3
Enzo Dayrit <enzodayrit11@gmail.com>
2013-05-28 05:28:21 +0900

    Automates header a lil

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,19 +1,25 @@
 class PageController < ApplicationController
   def home
+    @current = "home"
   end
 
   def about
+    @current = "about"
   end
 
   def updates
+    @current = "updates"
   end
 
   def application
+    @current = "application"
   end
 
   def travel
+    @current = "travel"
   end
 
   def contact
+    @current = "contact"
   end
 end
