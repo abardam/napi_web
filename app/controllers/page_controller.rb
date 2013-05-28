964914677b7abb984d93265e052d073498fe34a9
Enzo Dayrit <enzodayrit11@gmail.com>
2013-05-28 17:20:54 +0900

    Adds rudimentary database n stuff

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,4 +1,5 @@
 class PageController < ApplicationController
+
   def home
     @current = "home"
   end
--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -1,2 +1,13 @@
 module PageHelper
+  def footer_images
+    (Dir.entries("./app/assets/images/footer") - [".", ".."]).shuffle[0..3]
+  end
+  
+  def name_list
+    ["home", "about", "updates", "application", "travel", "contact"]
+  end
+  
+  def load_post(name)
+    Content.where("title = ?", name)[0].post
+  end
 end
